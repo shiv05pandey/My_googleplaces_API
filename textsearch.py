4e4ce60524647d2730e01dc864ab5c6e9a04aa6a
shivendra05_90 <shivendrapandey786@gmail.com>
2016-01-20 04:21:37 -0800

    Update textsearch.py

--- textsearch.py
+++ textsearch.py
@@ -2,7 +2,7 @@ import urllib
 import xml.etree.ElementTree as ET
 
 #                     Text Search Requests
-url1= 'https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+bangalore&key=AIzaSyDD0dXfpuFSbpGbcFr28w7-IeElAYaBOok'
+url1= 'https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+bangalore&key=API_KEY'
 resp1 = urllib.urlopen(url1)
 a1 =resp1.read()
 print a1
